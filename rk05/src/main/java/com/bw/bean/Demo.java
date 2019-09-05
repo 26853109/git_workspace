01cfaaf667e3114e291bb7b67447530989ced38e
T.lenovo <T.lenovo@LAPTOP-EJ5L8PJP>
2019-09-05 08:27:21 +0800

    第一次提交

---
+++ rk05/src/main/java/com/bw/bean/Demo.java
@@ -0,0 +1,16 @@
+/**
+ * Create Date:2019年9月5日
+ */
+package com.bw.bean;
+
+/**
+ * <br>Title:TODO 类标题
+ * <br>Description:TODO 类功能描述
+ * <br>Author:唐缘缘
+ * <br>Date:2019年9月5日
+ */
+public class Demo {
+  public static void main(String[] args) {
+    System.out.println("helloWord");
+  }
+}
